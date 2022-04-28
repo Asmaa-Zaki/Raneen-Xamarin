cec6740b61ca5411b40d54f2d1aa02218d06a738
Asmaa-Zaki <asmaazaki886@gmail.com>
2022-04-28 05:35:24 +0200

    account design

--- Raneen/Raneen/App.xaml.cs
+++ Raneen/Raneen/App.xaml.cs
@@ -1,9 +1,14 @@
-﻿using Raneen.Services;
+using Raneen.Services;
 using Raneen.Views;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+[assembly: ExportFont("Montserrat-Bold.ttf",Alias="Montserrat-Bold")]
+     [assembly: ExportFont("Montserrat-Medium.ttf", Alias = "Montserrat-Medium")]
+     [assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Montserrat-Regular")]
+     [assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "Montserrat-SemiBold")]
+     [assembly: ExportFont("UIFontIcons.ttf", Alias = "FontIcons")]
 namespace Raneen
 {
     public partial class App : Application
---
+++ Raneen/Raneen/Views/accountPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Raneen.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class accountPage : ContentPage
+    {
+        public accountPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void logIn(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SimpleLoginPage());
+        }
+    }
+}
