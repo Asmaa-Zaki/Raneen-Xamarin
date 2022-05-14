efd42b6fa2ed64d447170a3ed650dc7caf5050a2
Asmaa-Zaki <asmaazaki886@gmail.com>
2022-05-14 14:17:14 +0200

    Address

---
+++ Raneen/Raneen/Views/AddressPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace Raneen.Views
+{
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddressPage : ContentPage
+    {
+        public AddressPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void showCity(object sender, System.EventArgs e)
+        {
+            City.IsVisible= true;
+        }
+    }
+}
