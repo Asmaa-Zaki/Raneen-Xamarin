e29428b5487bd16da255a0c6b9b963ce6a1bc4ac
rahma <Rahma.zakaria21@gmail.com>
2022-05-14 21:08:47 +0200

    Home

---
+++ Raneen/Raneen/Models/HomeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raneen.Models
+{
+
+    public class HomeData
+    {
+        public HomeModel data;
+    }
+
+    public class HomeModel
+    {
+        public List<BannerModel> banners;
+        public List<ProductModel> products;
+        public string ad { get; set; }
+    }
+
+}
